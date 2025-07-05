61d712da5377cd5f30c0541b3cde82460e39cd28
Aniket Paul <aniket.paul618@gmail.com>
2025-07-05 12:17:12 +0530

    react cache is next js

--- app/(users)/rendering/dynamic/page.jsx
+++ app/(users)/rendering/dynamic/page.jsx
@@ -1,12 +1,22 @@
 import { db } from "@/config/db.jsx"
+import { cache } from "react"
 
 export const dynamic = "force-dynamic"
 
 const DynamicPage = async() => {
-    const [doctors] = await db.execute("Select * from doctors")
-    console.log("dynamic doctors ",doctors)
+    const doctors = await getAllDoctors()
   return (
     <div>    
+        <DoctorsComponent doctors = {doctors}/>
+    </div>
+  )
+}
+
+export default DynamicPage
+
+const DoctorsComponent = async ()=>{
+    const doctors = await getAllDoctors()
+    return (
         <ul>
             {
                 doctors.map(doctor => (
@@ -14,8 +24,11 @@ const DynamicPage = async() => {
                 ))
             }
         </ul>
-    </div>
-  )
+    )
 }
 
-export default DynamicPage
+const getAllDoctors = cache(async() =>{
+    const [doctors] = await db.execute("Select * from doctors")
+    console.log("dynamic fetching doctors ")
+    return doctors
+})
